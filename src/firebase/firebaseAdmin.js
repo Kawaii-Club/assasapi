70c980436203020857d96dfcb4f379a9c22054f8
MarySoarez <mary.soarez@gmail.com>
2026-02-10 22:19:09 -0300

    h

--- src/firebase/firebaseAdmin.js
+++ src/firebase/firebaseAdmin.js
@@ -1,28 +1,39 @@
+// import admin from "firebase-admin";
+// import fs from "fs";
+// import os from "os";
+// import path from "path";
+
+// if (!admin.apps.length) {
+//   const tmpFile = path.join(os.tmpdir(), "firebase-service-account.json");
+
+//   // grava o JSON no arquivo temporário
+//   fs.writeFileSync(
+//     tmpFile,
+//     process.env.FIREBASE_SERVICE_ACCOUNT_JSON,
+//     { encoding: "utf-8" }
+//   );
+
+//   // 🔥 lê e converte em objeto
+//   const serviceAccount = JSON.parse(
+//     fs.readFileSync(tmpFile, "utf-8")
+//   );
+
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+
+//   console.log("🔥 Firebase Admin inicializado:", serviceAccount.project_id);
+// }
+
+// export const db = admin.firestore();
+// export const messaging = admin.messaging();
+// export default admin;
 import admin from "firebase-admin";
-import fs from "fs";
-import os from "os";
-import path from "path";
 
 if (!admin.apps.length) {
-  const tmpFile = path.join(os.tmpdir(), "firebase-service-account.json");
-
-  // grava o JSON no arquivo temporário
-  fs.writeFileSync(
-    tmpFile,
-    process.env.FIREBASE_SERVICE_ACCOUNT_JSON,
-    { encoding: "utf-8" }
-  );
-
-  // 🔥 lê e converte em objeto
-  const serviceAccount = JSON.parse(
-    fs.readFileSync(tmpFile, "utf-8")
-  );
-
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    projectId: "kawaii-clube-116a6",
   });
-
-  console.log("🔥 Firebase Admin inicializado:", serviceAccount.project_id);
 }
 
 export const db = admin.firestore();
