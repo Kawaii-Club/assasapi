33d0fcd64c64b304657fbc9ac45f8ed0c74d083d
MarySoarez <mary.soarez@gmail.com>
2026-02-20 22:16:46 -0300

    aa

--- src/firebase/firebaseAdmin.js
+++ src/firebase/firebaseAdmin.js
@@ -4,25 +4,15 @@ import os from "os";
 import path from "path";
 
 if (!admin.apps.length) {
-  const tmpFile = path.join(os.tmpdir(), "firebase-service-account.json");
+  if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT não definida");
+  }
 
-  fs.writeFileSync(
-    tmpFile,
-    process.env.FIREBASE_SERVICE_ACCOUNT,
-    { encoding: "utf-8" }
-  );
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-
-  const serviceAccount = JSON.parse(
-    fs.readFileSync(tmpFile, "utf-8")
-  );
-  console.log("🔑 Service Account JSON carregado do ambiente.",serviceAccount);
-  console.log("🔑 Service Account carregado:", serviceAccount.project_id);
- 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-  console.log(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
   console.log("🔥 Firebase Admin inicializado:", serviceAccount.project_id);
 }
